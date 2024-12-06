3d6ae4f91ac91c1fdab91b01d66615ea54bafd42
Chris J. Karr <chris@audacious-software.com>
2024-12-06 19:38:03 +0000

    Adding is_date template tag

--- templatetags/simple_dashboard.py
+++ templatetags/simple_dashboard.py
@@ -1,5 +1,7 @@
 # pylint: disable=line-too-long, no-member
 
+import datetime
+
 from django import template
 from django.template.loader import render_to_string
 
@@ -16,3 +18,25 @@ def simple_dashboard_active_signals(context):
     template_context['active_dashboard_signals'] = DashboardSignal.objects.filter(active=True).order_by('-priority', 'name')
 
     return render_to_string('simple_dashboard_active_signals.html', template_context)
+
+@register.filter
+def simple_dashboard_is_date(value):
+    try:
+        datetime.datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f%z')
+
+        return True
+    except ValueError:
+        pass
+    except TypeError:
+        pass
+
+    try:
+        datetime.datetime.strptime(value, '%Y-%m-%d %H:%M:%S.%f%z')
+
+        return True
+    except ValueError:
+        pass
+    except TypeError:
+        pass
+
+    return False
