be678c66b0b6e36f15c05410821348f4e6f200f0
Chris J. Karr <chris@audacious-software.com>
2025-04-10 07:25:36 -0500

    Added JSON fallback tag

--- templatetags/simple_dashboard.py
+++ templatetags/simple_dashboard.py
@@ -1,8 +1,10 @@
 # pylint: disable=line-too-long, no-member
 
+import json
+
 from django import template
 from django.template.loader import render_to_string
-
+from django.utils.html import mark_safe
 from ..models import DashboardSignal
 
 register = template.Library()
@@ -16,3 +18,7 @@ def simple_dashboard_active_signals(context):
     template_context['active_dashboard_signals'] = DashboardSignal.objects.filter(active=True).order_by('-priority', 'name')
 
     return render_to_string('simple_dashboard_active_signals.html', template_context)
+
+@register.filter
+def simple_dashboard_to_json(raw_obj):
+    return mark_safe(json.dumps(raw_obj))
